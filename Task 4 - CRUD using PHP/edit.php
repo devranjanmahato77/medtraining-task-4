8da4268aeee8ee48747aa29976634ae7dec09ffe
devranjanmahato77 <112979851+devranjanmahato77@users.noreply.github.com>
2022-09-19 14:36:34 +0530

    Update edit.php

--- Task 4 - CRUD using PHP/edit.php
+++ Task 4 - CRUD using PHP/edit.php
@@ -1,93 +1,44 @@
+
 <?php
-include("database.php");
-
-$query = "select * from student_id where dt=1";
-$data = mysqli_query($con,$query);
-$result = mysqli_fetch_assoc($data);
-
-
-$sn = $_GET['sn'];
-$nm=$_GET['nm'];
-$rn=$_GET['rn'];
-$c=$_GET['c'];
-$e=$_GET['e'];
-$b=$_GET['b'];
-$mob=$_GET['mob'];
-$dt=$_GET['dt'];
-$ad=$_GET['ad'];
-
-if(isset($_POST['update'])){
-
-
-    $nm = $_POST['fullname'];
-    $rn = $_POST['regno'];
-    $c = $_POST['college'];
-    $e= $_POST['email'];
-    $b = $_POST['branch'];
-    $mob = $_POST['mobile'];
-    $dt = $_POST['dob'];
-    $ad = $_POST['address'];
-    $img= $_POST['image'];
-    $res = $_POST['resume'];
-
-
-$value = ["name"=>$nm,"regno"=>$rn,"college"=>$c,"email"=>$e,"branch"=>$b,"mobile"=>$mob,"dob"=>$dt,"address"=>$ad,"image"=>$img,"resume"=>$res];
-
-
-
-
-if($obj->update("student_id",$value)){
-    echo "<h2>Data Inserted Successfully.</h2>";
-}else{
-    echo "<h2>Data is not Inserted Successfully.</h2>";
-}
-}
-
-
+include 'database.php';
+$obj = new database();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>STUDENT ID CARD </title>
-    <!-- <link ref="stylesheet" href="./style.css"> -->
-    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
 </head>
-
-
-
 <body>
-    
-    <table>
-        <tr>
-            <td>
-            <a href="display.php">Back</a>
+
+
+
+<a href="home.php">Back</a>
                 <h2><u>Student ID card form</u></h2>
                 <form method="post">
 
 
                     <div>
                         <label>Full name</label><br>
-                        <input type="text" name="fullname" value="<?php echo "$nm";?>"><br><br>
+                        <input type="text" name="fullname" placeholder="Full name"><br><br>
                     </div>
 
                     <div>
                         <label>Registaion no.</label><br>
-                        <input type="text" name="regno" value="<?php echo "$rn";?>"><br><br>
+                        <input type="text" name="regno" placeholder="1901090001"><br><br>
                     </div>
 
                     <div>
                         <label>Colege name</label><br>
-                        <input type="text" name="college" value="<?php echo "$c";?>"><br><br>
+                        <input type="text" name="college" placeholder="College name"><br><br>
                     </div>
 
                     <div>
                         <label>Email</label><br>
-                        <input type="email" name="email" value="<?php echo "$e";?>"><br><br>
+                        <input type="email" name="email" placeholder="abc@gmail.com"><br><br>
                     </div>
 
 
@@ -95,7 +46,6 @@ if($obj->update("student_id",$value)){
                     
                     <label>Branch</label>
                     <select name="branch">
-                        <option value="<?php echo "$b"?>"><?php echo "$b"?></option>
                         <option value="CSE">CSE</option>
                         <option value="ECE">ECE</option>
                         <option value="CST">CST</option>
@@ -108,36 +58,36 @@ if($obj->update("student_id",$value)){
 
                     <div><br>
                         <label>Mobile</label><br>
-                        <input type="number" name="mobile" value="<?php echo "$mob";?>"><br><br>
+                        <input type="number" name="mobile" placeholder="9876543210"><br><br>
                     </div>
 
                     <div>
                     <label>DOB</label>
-                    <input type="date" name="dob" value="<?php echo "$dt";?>">
+                    <input type="date" name="dob">
                     </div>
                     <br><br>
 
                     <div>
                     <label>Address</label>
-                    <textarea id="address" name="address" rows="4" cols="50"><?php echo "$ad";?></textarea>
+                    <textarea id="address" name="address" rows="4" cols="50"></textarea>
                     </div>
 
                     <div>
                         <label>Profile picture</label>
-                        <input type="file" name="image" accept=".png, .jpg" value="<?php echo "$img";?>">
+                        <input type="file" name="image" accept=".png, .jpg">
                     </div>
 
                     <div>
                     <label>Resume</label>
                     
-                    <input type="file" name="resume" value="<?php echo "$res";?>">
+                    <input type="file" name="resume">
                     
                     </div>
 
 
 
                     <div>
-                        <button type="submit" name="update">Update</button>
+                        <button type="submit" name="submit">Submit</button>
                         
                     </div>
 
@@ -145,15 +95,34 @@ if($obj->update("student_id",$value)){
 
 
                 </form>
-            </td>
+<?php
+if(isset($_POST['submit'])){
 
-            <!--  -->
-        </tr>
-    </table>
+    $fullname = $_POST['fullname'];
+        $regno = $_POST['regno'];
+        $college = $_POST['college'];
+        $email = $_POST['email'];
+        $branch = $_POST['branch'];
+        $mobile = $_POST['mobile'];
+        $dob = $_POST['dob'];
+        $address = $_POST['address'];
+        $image = $_POST['image'];
+        $resume = $_POST['resume'];
+
+        $value = ['name'=>'$fullname','regno'=>'$regno','college'=>'$college','email'=>'$email','branch'=>'$branch','mobile'=>'$mobile','dob'=>'$dob','address'=>'$address','image'=>'$image','resume'=>'$resume'];
+
+        if($obj->insert("student_id",$value)){
+            echo "<h2>Data Inserted Successfully.</h2>";
+        }else{
+            echo "<h2>Data is not Inserted Successfully.</h2>";
+        }
+}
+    
+?>
 
 
 
 
-    </body>
-
+    
+</body>
 </html>
